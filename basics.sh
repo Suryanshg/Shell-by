d9edb455677430b2e4d85d3820514fe6a43743df
Suryanshg <suryanshg47@gmail.com>
2021-12-27 04:11:37 -0500

    Added triangle.sh

--- basics.sh
+++ basics.sh
@@ -6,10 +6,70 @@
 # Author : Suryanshg
 
 # Example echo statement. Use -e for activating escape sequences and -n for disabling the automatic newline character at the end of a statement
-echo -e -n "Instructions script initiating\nYour Name: "
+echo -e -n "Basics script initiating\nYour Name: "
 
 # Store the value in a variable
-read USER
+read USER 
 
-# Welcome the user
-echo "Welcome $USER" 
+# Variables
+VAR1="WORLD"
+
+# You can make the variable read only by adding readonly before the name
+
+VAR1=$USER
+
+echo Hello $VAR1
+
+# Unsetting the variable
+unset VAR1
+
+# For loop example
+echo "Here are the numbers from 0 to 5 using for loop"
+
+for i in 0 1 2 3 4 5 
+do
+	echo $i
+done
+
+# While loop example
+echo "Here are the numbers from 0 to 5 using while loop"
+
+i=0
+while [ $i -le 5 ]
+do
+	echo $i
+	i=`expr $i + 1`
+done
+
+# Nested loop example
+echo -e -n "Enter the size of the number triangle:"
+
+read sizeTriangle
+
+echo "Here is the number triangle using nested loops."
+
+i=1
+while [ $i -le $sizeTriangle ]
+do
+	# Print the Left half of the triangle
+	j=1
+	
+	while [ $j -lt $i ]
+	do
+		echo -n "$j "
+		j=`expr $j + 1`
+	done
+
+	# Print the Right half of the triangle
+	j=$i
+	while [ $j -gt 0 ]
+	do
+		echo -n "$j "
+		j=`expr $j - 1`
+	done 
+	echo
+	i=`expr $i + 1`
+done
+
+
+
---
+++ triangle.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Author: Suryanshg
+
+#echo -e -n "Enter the size of the number triangle:"
+
+#read sizeTriangle
+
+#echo "Here is the number triangle using nested loops."
+
+sizeTriangle=7
+
+sizeTriangle=$1
+
+i=1
+while [ $i -le $sizeTriangle ]
+do
+	# Print the needful spaces before the Left triangle
+	j=`expr $sizeTriangle - $i`
+	
+	while [ $j -gt 0 ]
+	do
+		echo -n "  "
+		j=`expr $j - 1`
+	done
+
+
+	
+        # Print the Left half of the triangle
+        j=1
+
+        while [ $j -lt $i ]
+        do
+                echo -n "$j "
+                j=`expr $j + 1`
+        done
+
+        # Print the Right half of the triangle
+        j=$i
+        while [ $j -gt 0 ]
+        do
+                echo -n "$j "
+                j=`expr $j - 1`
+        done
+        echo
+        i=`expr $i + 1`
+done
